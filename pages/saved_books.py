2467c3fdcc813dc9263f3c1c5ad752575990e9b9
Zmerrill1 <zach_merrill1@outlook.com>
2025-02-02 19:30:46 -0700

    Cover Image URL

--- pages/saved_books.py
+++ pages/saved_books.py
@@ -4,6 +4,7 @@ from decouple import config
 
 API_URL = config("API_URL")
 SAVED_BOOKS_URL = f"{API_URL}/books/"
+BOOK_COVER_URL = "https://books.google.com/books/content?id={bookid}&printsec=frontcover&img=1&zoom=1&source=gbs_gdata"
 
 st.title("Saved Books")
 
@@ -13,8 +14,10 @@ if response.status_code == 200:
 
     if saved_books:
         for book in saved_books:
-            st.image(book.cover_image_url, width=150, caption=book["title"])
             st.subheader(book["title"])
+            if "bookid" in book and book["bookid"]:
+                cover_image_url = BOOK_COVER_URL.format(bookid=book["bookid"])
+            st.image(cover_image_url, width=150, caption=book["title"])
             st.write(f"**Authors:** {book['authors']}")
             st.write(f"**Published Date:** {book.get('published_date', 'N/A')}")
 
