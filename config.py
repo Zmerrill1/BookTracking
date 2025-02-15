2dd3059903824fd7b08ca9866a422374d6e122e7
Zmerrill1 <zach_merrill1@outlook.com>
2025-02-15 16:37:14 -0700

    Fix: Updated config.py to use streamlit secrets

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from pydantic_settings import BaseSettings
 
 
@@ -18,6 +19,21 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+if "database" in st.secrets:
+    settings = Settings(
+        SECRET_KEY=st.secrets["security"]["SECRET_KEY"],
+        ALGORITHM=st.secrets["security"]["ALGORITHM"],
+        ACCESS_TOKEN_EXPIRE_MINUTES=st.secrets["security"][
+            "ACCESS_TOKEN_EXPIRE_MINUTES"
+        ],
+        API_URL=st.secrets["api"]["API_URL"],
+        OPENAI_API_KEY=st.secrets["openai"]["OPENAI_API_KEY"],
+        DATABASE_URL=st.secrets["database"]["DATABASE_URL"],
+        POSTGRES_USER=st.secrets["database"]["POSTGRES_USER"],
+        POSTGRES_PASSWORD=st.secrets["database"]["POSTGRES_PASSWORD"],
+        POSTGRES_DB=st.secrets["database"]["POSTGRES_DB"],
+    )
 
 if settings.DATABASE_URL.startswith("postgres://"):
     settings.DATABASE_URL = settings.DATABASE_URL.replace(
