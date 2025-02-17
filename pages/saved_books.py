8b463034b29e24cc0dbb219aad1417c9d2773b75
Zmerrill1 <zach_merrill1@outlook.com>
2025-02-16 17:23:16 -0700

    Add debugging

--- pages/saved_books.py
+++ pages/saved_books.py
@@ -14,6 +14,7 @@ BOOK_COVER_URL = "https://books.google.com/books/content?id={bookid}&printsec=fr
 
 
 st.title("📚 Saved Books")
+st.write(f"User ID: {st.session_state.get('user_id')}")
 
 # Ensure session state variables exist
 st.session_state.setdefault("access_token", None)
